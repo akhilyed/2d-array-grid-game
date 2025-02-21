a9ca1e7a30ea4360ef164ce992f8cbb932e922a1
akhilyed <158219372+akhilyed@users.noreply.github.com>
2025-02-21 10:42:09 -0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -3,19 +3,31 @@
 //2d arrays console game
 //Software design
 //Extra:Different types of levels
+#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
+
+const char PLAYER = 'P';
+const char TRESURE = 'T';
+const char MONSTER = 'M';
+const char GROUND = 'G';
+const char WATER = 'W';
+const char ROCKS = 'R';
+const int MAX_HEALTH = 3;
+
 int main() {
     char rules;
     string cont;
     int chest_amt;
     bool boat = false;
     bool gear = false;
+    bool water = false;
+    bool mount = false;
     cout << "Welcome dungeon explorer! Would you like to read the rules? (y/n): ";
     cin >> rules;
-
 
     if (rules == 'y' || rules == 'Y') {
         do {
@@ -26,7 +38,7 @@ int main() {
                  << "'G' spaces are ground spots in which the player can move to.\n\n"
                  << "'W' spaces are water spots, which cannot be crossed until the player gets a boat, which can be claimed randomly from a treasure chest. If the player does not have a boat and touches a water spot, they will respawn to the closest place they can and lose one health.\n\n"
                  << "'M' spaces are monsters which will make you fight with them if they are ever in the cardinal directions of the player. Players must kill them before they kill the player or they will lose.\n\n"
-                 << "'H' spaces are rocky spots in which you need climbing gear to cross over. Trying to cross over without it causes you to lose one health.\n\n"
+                 << "'R' spaces are rocky spots in which you need climbing gear to cross over. Trying to cross over without it causes you to lose one health.\n\n"
                  << "Enter 'understood' to start playing: ";
             cin.ignore(); // Clear the newline character from the input buffer
             getline(cin, cont);
@@ -34,7 +46,6 @@ int main() {
     } else {
         cout << "Ok then meanie >:(" << endl;
     }
-
 
     int diff;
     // Options for game
@@ -46,34 +57,91 @@ int main() {
          << "Dungeon Choice: ";
     cin >> diff;
 
+    char** dungeon = nullptr;
+    int size = 0;
 
     // Switch statement
     switch (diff) {
         case 1:
             cout << "Welcome to the Abyssal Crypt, a dungeon buried deep beneath an ancient ruin. Though no one has ever seen it, rumors say there lies a deep curse that seems to enchant everything that lies there, for the good or bad." << endl;
-            char dungeon[6][6];
+            size = 6;
             chest_amt = 4;
             break;
         case 2:
             cout << "Welcome to the Darkreach Catacombs where life is taken and remade into something more ... sinister." << endl;
-            char dungeon[9][9];
+            size = 9;
             chest_amt = 5;
             boat = true;
+            water = true;
             break;
         case 3:
             cout << "Welcome to the Forsaken Vaults is supposedly a place for those deemed too dangerous and volatile for the real world to see. However, the real reason behind the creation of the vaults remains a mystery." << endl;
-            char dungeon[12][12];
+            size = 12;
             chest_amt = 6;
             boat = true;
             gear = true;
+            water = true;
+            mount = true;
             break;
         default:
-            cout << "Invalid choice. Please select a valid dungeon." << endl;
-            break;
+            cout << "You accidentally selected an invalid option and died of boredom, bye! :)" << endl;
+            return 0;
+    }
+    dungeon = new char*[size];
+    for (int i = 0; i < size; ++i) {
+        dungeon[i] = new char[size];
+    }
+    srand(static_cast<unsigned int>(time(0)));
+    for(int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            int randTile = rand() % 6;
+            switch (randTile) {
+                case 0:
+                    dungeon[i][j] = GROUND; // Ground
+                    break;
+                case 1:
+                    if(water){
+                        dungeon[i][j] = WATER;
+                    } 
+                    else{
+                        dungeon[i][j] = GROUND; 
+                    }
+                    break;
+                case 2:
+                    if(mount){
+                    dungeon[i][j] = ROCKS; 
+                    }
+                    else{
+                        dungeon[i][j] = GROUND;
+                    }
+                    break;
+                case 3:
+                    dungeon[i][j] = MONSTER; 
+                    break;
+                case 4:
+                if(chest_amt != 0){
+                    dungeon[i][j] = TRESURE;
+                    chest_amt-= 1;
+                }
+                else{
+                    dungeon[i][j] = GROUND;
+                }
+                    break;
+                case 5:
+                    dungeon[i][j] = GROUND; 
+                    break;
+            }
+        }
     }
 
-
-
-
-    return 0;
+    dungeon[0][0] = PLAYER;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+        cout << dungeon[i][j] << ' ';
+        }
+    cout << endl; 
+    }
+    return 0; 
 }
